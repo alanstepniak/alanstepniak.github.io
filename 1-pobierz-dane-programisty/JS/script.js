090a66f4ef1a749affdc9e888dc2c85c50a5c2e7
alanstepniak <99657826+alanstepniak@users.noreply.github.com>
2022-03-29 12:20:09 +0200

    poprawka

--- 1-pobierz-dane-programisty/JS/script.js
+++ 1-pobierz-dane-programisty/JS/script.js
@@ -5,10 +5,20 @@ $(document).ready(function () {
     $(btn).click(function () {
         $.ajax({
             url: 'https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php',
-            statusCode: {
-                200: function (data) {
-                    wynik.innerText = data;
-                }
+            success: (data) => {
+                let object = JSON.parse(data);
+
+                let name = $('<p></p>').text(`Imię: ${object.imie}`);
+                let lastName = $('<p></p>').text(`Nazwisko: ${object.nazwisko}`);
+                let occupation = $('<p></p>').text(`Zawód: ${object.zawod}`);
+                let company = $('<p></p>').text(`Firma: ${object.firma}`);
+
+                let container = $('#dane-programisty');
+
+                container.append(name);
+                container.append(lastName);
+                container.append(occupation);
+                container.append(company);
             }
         })
     })
